a14739f51811af17a1d925ec80348f29083a94b4
mr ranvijay <ranvijay.cse@outlook.com>
2018-08-16 21:39:37 +0530

    commit before creating basic shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BlogViewComponent } from './blog-view/blog-view.component';
+import { BlogCreateComponent } from './blog-create/blog-create.component';
+import { BlogEditComponent } from './blog-edit/blog-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogViewComponent,
+    BlogCreateComponent,
+    BlogEditComponent
   ],
   imports: [
     BrowserModule
